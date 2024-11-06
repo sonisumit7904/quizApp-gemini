bb485390b118edd2e0e37e2123227b156d712aa4
sonisumit7904 <sonisumit7904@gmail.com>
2024-11-06 17:30:16 +0530

    completed gemini

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // const targetUrl = 'http://localhost:8080';
 const targetUrl = 'https://quiz2-utkarsh.up.railway.app';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_GOOGLE_API_KEY': JSON.stringify(process.env.VITE_GOOGLE_API_KEY)
+  },
   server: {
     proxy: {
       '/Quiz': {
